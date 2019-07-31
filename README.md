dd5cc3b2045fc840fac56974e2f093b540f3f25d
Zhi Yang Ten <tenzy@seagroup.com>
2019-07-31 12:04:13 +0800

    ! added readme and updated code
    ! added gitignore
    
    ! removed ds store

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pokemon speech generator
+
+![2gram](./2gram.png)
+
+Modified a [markov chain tutorial](https://medium.com/@corrigan1247/how-to-imitate-trump-with-markov-chains-8224877dcf69) to take in n-grams instead of just bi-grams and using [1mb worth of speeches](https://github.com/ryanmcdermott/trump-speeches) from [@ryconoclast](https://twitter.com/ryconoclast) to generate new content and potentially tweets
--- index.js
+++ index.js
@@ -18,24 +18,39 @@ function chooseStartingToken() {
 }
 
 
-function findNextWord(currentWord) {
+function findNextWord(prevWords) {
     var nextWords = [];
     for (var w = 0; w < tokens.length - 1; w++) {
-        if (tokens[w] == currentWord) {
-            nextWords.push(tokens[w + 1]);
-        }
+        if (prevWords.length > 0 && w - prevWords.length > 0) {
+            let matchNgram = true;
+            for (let i = 0; i < prevWords.length; i++) {
+                if (tokens[w - i] != prevWords[prevWords.length - 1 -i]) {
+                    matchNgram = false;
+                    break;
+                }
+            }
+            if (matchNgram === true) {
+                nextWords.push(tokens[w + 1]);
+            }
+        } 
     }
     var word = nextWords[Math.floor(Math.random() * nextWords.length)]; // choose a random next word
     return word;
 }
 
-function generate(num) {
+function generate(len, gramlen) {
     var currentWord = chooseStartingToken();
+    var prev = [];
+    prev.push(currentWord);
     var sentence = currentWord + " ";
     var wordcount = 0;
-    while (wordcount < num || currentWord.indexOf(".") < 0) { // while we haven't found a period
-        currentWord = findNextWord(currentWord);
+    while (wordcount < len || currentWord.indexOf(".") < 0) { // while we haven't found a period
+        currentWord = findNextWord(prev);
         sentence += currentWord + " ";
+        prev.push(currentWord);
+        if (prev.length > gramlen) {
+            prev.shift();
+        }
         wordcount++;
     }
     // return sentence;
@@ -46,16 +61,18 @@ function generate(num) {
 
 function start() {
     const url = './speeches.txt';
+    const GRAM_LEN = 2;
+    const OUT_MIN_LEN = 20;
     fetch(url)
         .then(function (resp) {
             console.log('loading');
             resp.text().then(speech => {
                 trump_speeches = speech;
                 createTokens();
-                generate(20);
-                console.log('loaded');
+                generate(OUT_MIN_LEN, GRAM_LEN);
+                console.log(`loaded, length ${OUT_MIN_LEN}, ${GRAM_LEN}-gram`);
             })
-            document.getElementById("generate").addEventListener("click", () => generate(20));
+            document.getElementById("generate").addEventListener("click", () => generate(OUT_MIN_LEN, GRAM_LEN));
         })
         .catch(function (error) {
             console.log(error);
